0c145af2dabcec42080b303bd875231123614bc5
BuildTools <unconfigured@null.spigotmc.org>
2020-01-06 20:02:23 -0500

    CleanChat helper fix - now removes bot and cmd spam!

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', async message => {
     if (message.author.id == '159985870458322944')
         message.channel.send('FUCK OFF MEE6, YOU STUPID BITCH :dagger: :knife: :fire:');
 
-    //common.bot.reactions(message);
+        //common.bot.reactions(message);
 
     if (!common.config.settings.prefix.includes(message.content.charAt(0)) || message.author.bot) return;
 
