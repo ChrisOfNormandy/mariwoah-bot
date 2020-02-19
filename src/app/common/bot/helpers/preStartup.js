21081e090a48bc7e3aa169f4d767b9ea39994138
Chris Hinton <tvvc.chris@gmail.com>
2020-02-19 16:55:49 -0500

    Part 1: Delete | Moving helper files to categories.

--- src/app/common/bot/helpers/preStartup.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config');
-const Discord = require('discord.js');
-
-module.exports = function () {
-    const client = new Discord.Client();
-    client.token = config.auth.token;
-    return client;
-}
