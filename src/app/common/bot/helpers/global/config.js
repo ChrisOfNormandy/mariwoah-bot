00c055b12f3a1c96646422187f9f6c3a7cccd70c
Chris Hinton <tvvc.chris@gmail.com>
2020-02-25 11:51:12 -0500

    Token

--- src/app/common/bot/helpers/global/config.js
+++ src/app/common/bot/helpers/global/config.js
@@ -5,7 +5,7 @@ module.exports = {
         logChannel: "643564636326592523"
     },
     auth: {
-        token: "NjM3NzU3NDQwNjYxOTEzNjIw.XlGPOg.dmkoGXPFnfJGOwwbXoXUd6cXu08"
+        token: "NjM0OTM2MjA3MzcyOTc2MTM2.XlVQWA.YHOsOb3lkFyc_2WKZ4k9Svr2jug"
     },
     admins: {
         "188020615989428224": true
