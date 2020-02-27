ae19bc8d9756af361805849aa73306f216ef8db1
Chris Hinton <tvvc.chris@gmail.com>
2020-02-27 12:50:26 -0500

    rewritten for reasons

--- src/app/common/roleManager/helpers/users/userInfoList.js
+++ src/app/common/roleManager/helpers/users/userInfoList.js
@@ -1,85 +1,33 @@
+const chatFormat = require('../../../bot/helpers/global/chatFormat');
 const Discord = require('discord.js');
 const getUser = require('./getUser');
+
+function getList(message, userID, discord_user, user, operation) {
+    let embedMsg = new Discord.RichEmbed();
+    if (user.data[operation].length) {
+        embedMsg.setTitle((discord_user) ? `${discord_user.username}#${discord_user.discriminator} - ${operation}` : `${userID} - ${operation}`);
+        embedMsg.setColor(chatFormat.colors.information);
+        
+        let obj;
+        for (let i = 0; i < user.data[operation].length; i++) {
+            obj = user.data[operation][i];
+            embedMsg.addField(
+                `${i + 1} - ${message.channel.guild.members.get(obj.staffID).user.username}`,
+                `Reason: "${obj.reason || 'No reason given'}" on ${obj.date.day} at ${obj.date.time}.
+                ${(obj.pardoned.value) ? `Pardoned by ${message.channel.guild.members.get(obj.pardoned.staffID).user.username}.\nReason: "${obj.pardoned.reason || 'No reason given'}" on ${obj.pardoned.date.day} at ${obj.pardoned.date.time}.` : ''}`
+            );
+        }
+    }
+    else
+        return `User doesn't have any ${operation} on record.`;
+    return embedMsg;
+}
 
 module.exports = function (message, userID, listName) {
     getUser(message, userID)
         .then(user => {
             let discord_user = (message.channel.guild.members.get(userID)) ? message.channel.guild.members.get(userID).user : null;
-
-            let embedMsg = new Discord.RichEmbed();
-
-            switch (listName) {
-                case 'warnings': {
-                    if (user.data.warnings.length) {
-                        embedMsg.setTitle((discord_user) ? discord_user.username + '#' + discord_user.discriminator + ' - Warnings' : `${userID} - Warnings`);
-
-                        let obj;
-                        for (let i in user.data.warnings) {
-                            obj = user.data.warnings[i];
-                            embedMsg.addField(
-                                `${i + 1} - ${message.channel.guild.members.get(obj.staffID).user.username}`,
-                                `Reason: "${obj.reason || 'No reason given'}" on ${obj.date.day} at ${obj.date.time}.`
-                            );
-                        }
-                    }
-                    else
-                        return message.channel.send(`User doesn't have any warnings on record.`);
-                    break;
-                }
-                case 'kicks': {
-                    if (user.data.kicks.length) {
-                        embedMsg.setTitle((discord_user) ? discord_user.username + '#' + discord_user.discriminator + ' - Kicks' : `${userID} - Kicks`);
-
-                        let obj;
-                        for (let i in user.data.kicks) {
-                            obj = user.data.kicks[i];
-                            embedMsg.addField(
-                                `${i + 1} - ${message.channel.guild.members.get(obj.staffID).user.username}`,
-                                `Reason: "${obj.reason || 'No reason given'}" on ${obj.date.day} at ${obj.date.time}.`
-                            );
-                        }
-                    }
-                    else
-                        return message.channel.send(`User doesn't have any kicks on record.`);
-                    break;
-                }
-                case 'bans': {
-                    if (user.data.bans.length) {
-                        embedMsg.setTitle((discord_user) ? discord_user.username + '#' + discord_user.discriminator + ' - Bans' : `${userID} - Bans`);
-
-                        let obj;
-                        for (let i in user.data.bans) {
-                            obj = user.data.bans[i];
-                            embedMsg.addField(
-                                `${i + 1} - ${message.channel.guild.members.get(obj.staffID).user.username}`,
-                                `Reason: "${obj.reason || 'No reason given'}" on ${obj.date.day} at ${obj.date.time}.`
-                            );
-                        }
-                    }
-                    else
-                        return message.channel.send(`User doesn't have any bans on record.`);
-                    break;
-                }
-                case 'banReverts': {
-                    if (user.data.banReverts.length) {
-                        embedMsg.setTitle((discord_user) ? discord_user.username + '#' + discord_user.discriminator + ' - Ban Reverts' : `${userID} - Ban Reverts`);
-
-                        let obj;
-                        for (let i in user.data.banReverts) {
-                            obj = user.data.banReverts[i];
-                            embedMsg.addField(
-                                `${i + 1} - ${message.channel.guild.members.get(obj.staffID).user.username}`,
-                                `Reason: "${obj.reason || 'No reason given'}" on ${obj.date.day} at ${obj.date.time}.`
-                            );
-                        }
-                    }
-                    else
-                        return message.channel.send(`User doesn't have any ban reverts on record.`);
-                    break;
-                }
-            }
-
-            message.channel.send(embedMsg);
+            message.channel.send(getList(message, userID, discord_user, user, listName));
         })
         .catch(e => console.log(e));
 }
